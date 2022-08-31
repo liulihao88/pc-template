b068565b40c1c823e5d550e099c26dd6bca2f359
刘力豪 <18311056454@sina.cn>
2022-08-31 20:13:51 +0800

    FEATURE:提交啊

--- src/views/testPage/t2.vue
+++ src/views/testPage/t2.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <el-button @click="t1" type="primary">测试1</el-button>
-    <el-button @click="t2" type="info">测试222</el-button>
+    <el-button @click="t2" type="info">测试2322</el-button>
   </div>
 </template>
 
